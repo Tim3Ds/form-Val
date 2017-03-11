b84fbf21c1e03cb1e09cd5ad562ce987b0d6771b
Tim3Ds <Tim3Ds@gmail.com>
2017-03-11 15:42:02 -0700

    working

--- src/App/Form/Form.js
+++ src/App/Form/Form.js
@@ -6,26 +6,75 @@ class Form extends React.Component{
         super(props);
         //bind the specific instance of onchange from
         //this particular class to the onchange method
-        this.onChange = this.onChange.bind(this);
         this.onSubmit = this.onSubmit.bind(this);
         this.onEnter = this.onEnter.bind(this);
 
         //set default form state
         this.state = {
-            name: 'your name',
-            email: 'your@email.com',
-            phone: '888-888-8888'
+            name: '',
+            email: '',
+            phone: '',
+            valid: false
             
         };
     }
 
-    onChange(event){
-
+    onChangeName(event){
+        let reg = /^[a-z ,.'-]+$/i
+        if(!reg.test(event.target.value)){
+            let elm = document.getElementById('name')
+            elm.className = 'error'
+            this.setState({
+                valid: false
+            })      
+        }else{
+            let elm = document.getElementById('name')
+            elm.className = 'good'
+            this.setState({
+                valid: true
+            })
+        }
         this.setState({
-            email: event.target.email,
-            name: event.target.name,
-            phone: event.target.phone
-        });
+            name: event.target.value
+        })
+    }
+    onChangeEmail(event){
+        let reg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+        if(!reg.test(event.target.value)){
+            let elm = document.getElementById('email')
+            elm.className = 'error'
+            this.setState({
+                valid: false
+            })    
+        }else{
+            let elm = document.getElementById('email')
+            elm.className = 'good'
+            this.setState({
+                valid: true
+            })
+        }
+        this.setState({
+            email: event.target.value
+        })
+    }
+    onChangePhone(event){
+        let reg = /^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$/
+        if(!reg.test(event.target.value)){
+            let elm = document.getElementById('phone')
+            elm.className = 'error'
+            this.setState({
+                valid: false
+            })   
+        }else{
+            let elm = document.getElementById('phone')
+            elm.className = 'good'
+            this.setState({
+                valid: true
+            })
+        }
+        this.setState({
+            phone: event.target.value
+        })
     }
 
     onEnter(event){
@@ -34,45 +83,59 @@ class Form extends React.Component{
         }
     }
 
-    onSubmit(){
-        //this.props.submitAction(this.state);
-        this.setState({
-            name:'',
-            email: '',
-            phone: ''
-        });
+    onSubmit(event){
+        event.preventDefault();
+        if(this.state.valid){
+
+            this.setState({
+                name:'',
+                email: '',
+                phone: ''
+            });
+        }
     }
 
     render(){
         return(
             <div className='form'>
-                <h3>Name</h3>
-                <input 
-                    type="text" 
-                    id='name'
-                    onChange={this.onChange}
-                    onKeyDown={this.onEnter}
-                    value={this.state.name}
-                    />
-                <h3>Email</h3>
-                <input 
-                    type="text" 
-                    id='email'
-                    onChange={this.onChange}
-                    onKeyDown={this.onEnter}
-                    value={this.state.email}
-                    />
-                <h3>Phone</h3>
-                <input 
-                    type="text" 
-                    id='phone'
-                    onChange={this.onChange}
-                    onKeyDown={this.onEnter}
-                    value={this.state.phone}
-                    />
-                <h3>
-                    <button onClick={this.onSubmit}>Submit</button>
-                </h3>
+                <tr>
+                    <td>Name</td>
+                    <td>
+                        <input 
+                            type="text" id="name"
+                            onChange={this.onChangeName.bind(this)}
+                            onKeyDown={this.onEnter}
+                            placeholder="Your Name"
+                            value={this.state.name}
+                            />
+                    </td>
+                </tr>
+                <tr>
+                    <td>Email</td>
+                    <td>
+                        <input 
+                            type="text" id='email'
+                            onChange={this.onChangeEmail.bind(this)}
+                            onKeyDown={this.onEnter}
+                            placeholder="your@email.address"
+                            value={this.state.email}
+                            />
+                    </td>
+                    
+                </tr>
+                <tr>
+                    <td>Phone</td>
+                    <td>
+                        <input 
+                            type="text" id='phone'
+                            onChange={this.onChangePhone.bind(this)}
+                            onKeyDown={this.onEnter}
+                            placeholder="***-***-****"
+                            value={this.state.phone}
+                            />
+                    </td>
+                </tr>
+                <button onClick={this.onSubmit}>Submit</button>
             </div>
         )
     }
