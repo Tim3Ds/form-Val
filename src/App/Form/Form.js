a9cb1e2a293b2ac38eac7fe247138070a0d02e55
Tim3Ds <Tim3Ds@gmail.com>
2017-03-14 22:44:46 -0600

    add action to button

--- src/App/Form/Form.js
+++ src/App/Form/Form.js
@@ -22,15 +22,16 @@ class Form extends React.Component{
                         reg = /^[a-z ,.'-]+$/i;
                         break;
                     case 'email':
-                        reg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+                        reg = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
                         break;
                     case 'phone':
-                        reg = /^[(]{0,1}[0-9]{3}[)]{0,1}[-\s\.]{0,1}[0-9]{3}[-\s\.]{0,1}[0-9]{4}$/
+                        reg = /^[(]{0,1}[0-9]{3}[)]{0,1}[-\s.]{0,1}[0-9]{3}[-\s.]{0,1}[0-9]{4}$/
                         break;
                     default:
                         break;
                 }
                 if(!reg.test(elm.value)){
+                    document.getElementById('Valid').innerHTML= ``
                     elm.className = 'error';
                     return false;
                 }else{
@@ -44,7 +45,6 @@ class Form extends React.Component{
     }
 
     onChangeName(event){
-        
         this.state.valid('name');
         this.setState({
             name: event.target.value
@@ -77,50 +77,56 @@ class Form extends React.Component{
                 email: '',
                 phone: ''
             });
+            document.getElementById('Valid').innerHTML= `<p>Your input was Valid</p>`
         }
     }
 
     render(){
         return(
-            <div className='form'>
-                <tr>
-                    <td>Name</td>
-                    <td>
-                        <input 
-                            type="text" id="name"
-                            onChange={this.onChangeName.bind(this)}
-                            onKeyDown={this.onEnter}
-                            placeholder="Your Name"
-                            value={this.state.name}
-                            />
-                    </td>
-                </tr>
-                <tr>
-                    <td>Email</td>
-                    <td>
-                        <input 
-                            type="text" id='email'
-                            onChange={this.onChangeEmail.bind(this)}
-                            onKeyDown={this.onEnter}
-                            placeholder="your@email.address"
-                            value={this.state.email}
-                            />
-                    </td>
-                    
-                </tr>
-                <tr>
-                    <td>Phone</td>
-                    <td>
-                        <input 
-                            type="text" id='phone'
-                            onChange={this.onChangePhone.bind(this)}
-                            onKeyDown={this.onEnter}
-                            placeholder="***-***-****"
-                            value={this.state.phone}
-                            />
-                    </td>
-                </tr>
-                <button onClick={this.onSubmit}>Submit</button>
+            <div>
+                <div className='form'>
+                    <tr>
+                        <td>Name</td>
+                        <td>
+                            <input 
+                                type="text" id="name"
+                                onChange={this.onChangeName.bind(this)}
+                                onKeyDown={this.onEnter}
+                                placeholder="Your Name"
+                                value={this.state.name}
+                                />
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>Email</td>
+                        <td>
+                            <input 
+                                type="text" id='email'
+                                onChange={this.onChangeEmail.bind(this)}
+                                onKeyDown={this.onEnter}
+                                placeholder="your@email.address"
+                                value={this.state.email}
+                                />
+                        </td>
+                        
+                    </tr>
+                    <tr>
+                        <td>Phone</td>
+                        <td>
+                            <input 
+                                type="text" id='phone'
+                                onChange={this.onChangePhone.bind(this)}
+                                onKeyDown={this.onEnter}
+                                placeholder="***-***-****"
+                                value={this.state.phone}
+                                />
+                        </td>
+                    </tr>
+                    <button onClick={this.onSubmit}>Submit</button>
+                </div>
+                <div id="Valid">
+
+                </div>
             </div>
         )
     }
